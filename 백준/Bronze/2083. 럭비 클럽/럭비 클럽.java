8b365ec784d327af2ccd0484f1a25c5c6f16038e
sung <81069048+lolu1032@users.noreply.github.com>
2025-07-14 22:31:29 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()) {
+            String str = sc.next();
+            int age = sc.nextInt();
+            int weight = sc.nextInt();
+
+            if(str.equals("#") && age == 0 && weight == 0) {
+                return ;
+            }
+
+            if(age > 17 || weight >= 80) {
+                System.out.println(str + " Senior");
+            }else{
+                System.out.println(str + " Junior");
+            }
+        }
+    }
+}

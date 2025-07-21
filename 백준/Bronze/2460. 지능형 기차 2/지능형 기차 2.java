77847e05f8fb17ff9b890a36880fddb138e6d766
sung <81069048+lolu1032@users.noreply.github.com>
2025-07-21 22:31:59 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 176 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int max = -1;
+        for(int i = 0; i < 10; i++) {
+            int n = sc.nextInt(); // 내린 사람
+            int m = sc.nextInt(); // 탄 사람
+
+            count = count - n + m;
+            if(max  < count) {
+                max = count;
+            }
+        }
+        System.out.println(max);
+    }
+}

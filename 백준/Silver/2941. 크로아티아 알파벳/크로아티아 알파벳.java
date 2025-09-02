3f9162d527c648f4257d16ed95d7bd45a728685d
sung <81069048+lolu1032@users.noreply.github.com>
2025-09-02 23:54:16 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = {"dz=","c=","c-","d-","lj","nj","s=","z="};
+
+        String str = sc.next();
+        for(int i = 0; i < arr.length; i++) {
+            if(str.contains(arr[i])) {
+                str = str.replace(arr[i],"0");
+            }
+        }
+
+        System.out.println(str.length());
+    }
+}
